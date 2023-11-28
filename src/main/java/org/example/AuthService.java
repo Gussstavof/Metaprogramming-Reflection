dad38840462204310bda97b437b3f54daa195a39
Gussstavof <g717397@gmail.com>
2023-11-28 19:28:52 -0300

    fix: Dependency Injection (class and interface)

---
+++ src/main/java/org/example/AuthService.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class AuthService {
+
+    public boolean auth(User user){
+        return user.getName() != null;
+    }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,18 +1,16 @@
 package org.example;
 
-import java.lang.annotation.Annotation;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
+import org.reflections.Reflections;
+
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
         UserService service = new UserService();
-
-        Map<Class<?>, Object> dependencies = new HashMap<>();
-
-        dependencies.put(DBRepository.class, new FakeDBRepository());
-        dependencies.put(AuthService.class, new AuthService());
+        List<Class<?>> dependencies = new ArrayList<>(){{
+            add(DBRepository.class);
+            add(AuthService.class);
+        }};
 
         dependencyInjection(service, dependencies);
 
@@ -35,27 +33,47 @@ public class Main {
          */
     }
 
-    public static void dependencyInjection(Object obj, Map<Class<?>, Object> dependencies){
-        dependencies.forEach((type, object) -> {
-            Arrays.stream(obj.getClass().getDeclaredFields())
-                    .filter( field -> field.isAnnotationPresent(Injection.class) &&
-                            (
-                                    Arrays.stream(object.getClass().getAnnotatedInterfaces())
-                                    .anyMatch(annotatedType ->
-                                            annotatedType.equals(field.getAnnotatedType())
-                                    ) ||
-                                    field.getAnnotatedType().toString()
-                                            .equals(object.getClass().getTypeName())
-                            )
-                    )
-                    .forEach(field -> {
-                        try {
-                            field.setAccessible(true);
-                            field.set(obj, object);
-                        } catch (IllegalAccessException e) {
-                            throw new RuntimeException(e);
-                        }
-                    });
+    public static void dependencyInjection(Object obj, List<Class<?>> dependencies) {
+        dependencies.forEach((type) -> {
+            Object object;
+            try {
+                if (type.isInterface()) {
+                    object = searchImpl(type, "org.example");
+                } else {
+                    object = type.getConstructor().newInstance();
+                }
+
+                Arrays.stream(obj.getClass().getDeclaredFields())
+                        .filter(field -> field.isAnnotationPresent(Injection.class) &&
+                                (
+                                        Arrays.stream(object.getClass().getAnnotatedInterfaces())
+                                                .anyMatch(annotatedType ->
+                                                        annotatedType.equals(field.getAnnotatedType())
+                                                )
+                                                || field.getAnnotatedType().toString()
+                                                .equals(object.getClass().getTypeName())
+                                                || field.getAnnotatedType()
+                                                .equals(object.getClass().getAnnotatedSuperclass())
+                                )
+                        )
+                        .forEach(field -> {
+                            try {
+                                field.setAccessible(true);
+                                field.set(obj, object);
+                            } catch (IllegalAccessException e) {
+                                throw new RuntimeException(e);
+                            }
+                        });
+            } catch (Exception e) {
+                throw new RuntimeException(e.getMessage());
+            }
         });
+    }
+
+    private static Object searchImpl(Class<?> classAbs, String packageImpl) throws Exception {
+        return new Reflections(packageImpl).getSubTypesOf(classAbs).stream()
+                .findFirst()
+                .orElseThrow(RuntimeException::new)
+                .getConstructor().newInstance();
     }
 }
